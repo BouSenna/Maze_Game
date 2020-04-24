b2284fee2b5cea000f7c4e80504c5a48f29d42d5
BouSenna <49456936+BouSenna@users.noreply.github.com>
2020-04-24 03:24:09 +0200

    added predicate to replace a value in a list with 'X'

--- Maze.pl
+++ Maze.pl
@@ -46,3 +46,19 @@ move([[X, Y], Diamonds, Curr_Maze], [[New_X, Y], New_Diamonds, New_Maze]):-
 	(Val = 0 -> New_Diamonds is Diamonds ;
 			    New_Diamonds is Diamonds + 1),
 	replace(Curr_Maze, New_X, Y, New_Maze).
+
+% This predicates replaces the cell's value of the given row and column in the given maze with 'X'
+replace([H|T], 0, Column, [R|T]):-
+     replace_column(H, Column, 'X', R).  
+
+replace([H|T1], Row, Column, [H|T2]):- 
+     Row > 0 ,                              
+     Row1 is Row-1 ,                       
+     replace(T1 , Row1, Column, T2).         
+
+replace_column([_|T] , 0, Val, [Val|T]).  
+replace_column([H|T1], Column, Val, [H|T2]) :- 
+     Column > 0,                                
+     Column1 is Column-1,                                
+     replace_column(T1 , Column1, Val, T2).
+
